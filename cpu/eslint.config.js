1f3c47145d530f864cc23e180fb37b1ec0ae93bd
Sri Rangan <srirangan0603@gmail.com>
2026-04-22 18:46:27 +0530

    Update eslint.config.js

--- cpu/eslint.config.js
+++ cpu/eslint.config.js
@@ -4,8 +4,11 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
+
 export default defineConfig([
+
   globalIgnores(['dist']),
+  
   {
     files: ['**/*.{js,jsx}'],
     extends: [
