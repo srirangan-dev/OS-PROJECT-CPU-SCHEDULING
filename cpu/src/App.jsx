c797c0d3af1915a247a3fb385d6ba6f847a60aa7
Sri Rangan <srirangan0603@gmail.com>
2026-06-04 11:38:56 +0530

    Update App.jsx

--- cpu/src/App.jsx
+++ cpu/src/App.jsx
@@ -7,6 +7,9 @@ import StatsPanel from './components/StatsPanel';
 import { runCPUSimulation } from './utils/scheduler';
 import './App.css';
 
+
+
+
 const DEFAULT_PROCESSES = [
   { id: 'p1', pid: 'P1', arrival: 0, burst: 6, priority: 2 },
   { id: 'p2', pid: 'P2', arrival: 2, burst: 4, priority: 1 },
